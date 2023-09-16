4ee9cb26e0d31ba5f45335de42e7a0a515d42ce5
Steven Muñoz Vega <Darkmixt@gmail.com>
2023-09-15 22:58:10 -0600

    Search country Filter added

--- src/app/countries/countries-routing.module.ts
+++ src/app/countries/countries-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
-import { CountryPageComponent } from './pages/country-page/country-page.component';
+import { CountryPageComponent } from './pages/country-page/CountryPageComponent';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 
--- src/app/countries/pages/country-page/CountryPageComponent.ts
+++ src/app/countries/pages/country-page/CountryPageComponent.ts
@@ -1,57 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+
 
 @Component({
   selector: 'app-country-page',
   templateUrl: './country-page.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class CountryPageComponent implements OnInit {
 
   constructor(
     private countriesService: CountriesService,
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-    )
-    {}
-
+    private activatedRoute: ActivatedRoute) { }
 
 
   // ngOnInit(): void {
   //   this.activatedRoute.params
   //   .subscribe( ({id}) => { // podemos destructurar el params para dejarlo en solo ID params   =   ({ id })
   //     //console.log({params: id })
-
   //     this.countriesService.searchCountryByAlphaCode(id)
   //     .subscribe(country => {
   //       console.log({country})
   //     });
-
   //   });
   // }
- //! la funcion comentariada de arriba hace lo miismo que la de abajo
   ngOnInit(): void {
     this.activatedRoute.params
-    .pipe(
-      switchMap( ({id}) => this.countriesService.searchCountryByAlphaCode(id)),
-    )
-    .subscribe(country =>{
-
-      if ( !country){
-        return this.router.navigateByUrl('')
-      }
-
-      console.log('TENEMOS UN PAIS')
-      return
-    })
+      .pipe(
+        switchMap
+      );
   }
 
-
-
-
-
-
-  }
+}
